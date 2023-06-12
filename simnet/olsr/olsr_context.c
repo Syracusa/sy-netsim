8ab6681291ac1ecc31390a17d7fd825145dbfd1d
Syracusa <msrndsy@gmail.com>
2023-06-12 14:38:47 +0900

    OLSR Repos

--- simnet/olsr/olsr_context.c
+++ simnet/olsr/olsr_context.c
@@ -7,12 +7,19 @@ OlsrContext g_olsr_ctx;
 
 void init_olsr_param()
 {
-    OlsrParam* param = &g_olsr_ctx.param;
+    OlsrParam *param = &g_olsr_ctx.param;
 
     param->hello_interval_ms = DEF_HELLO_INTERVAL_MS;
     param->tc_interval_ms = DEF_TC_INTERVAL_MS;
     param->willingness = WILL_DEFAULT;
+}
 
+static int rbtree_compare_by_inetaddr(const void *k1, const void *k2)
+{
+    in_addr_t *n1 = (in_addr_t *)k1;
+    in_addr_t *n2 = (in_addr_t *)k2;
+
+    return *n1 - *n2;
 }
 
 void init_olsr_context(CommonRouteConfig *config)
@@ -22,4 +29,30 @@ void init_olsr_context(CommonRouteConfig *config)
     memcpy(&g_olsr_ctx.conf, config, sizeof(CommonRouteConfig));
     g_olsr_ctx.timerqueue = create_timerqueue();
     g_olsr_ctx.olsr_tx_msgbuf = RingBuffer_new(OLSR_TX_MSGBUF_SIZE);
+
+    g_olsr_ctx.neighbor_link_tree = rbtree_create(rbtree_compare_by_inetaddr);
+    g_olsr_ctx.neighbor_tree = rbtree_create(rbtree_compare_by_inetaddr);
+    g_olsr_ctx.mpr_tree = rbtree_create(rbtree_compare_by_inetaddr);
+    g_olsr_ctx.selector_tree = rbtree_create(rbtree_compare_by_inetaddr);
+    g_olsr_ctx.topology_tree = rbtree_create(rbtree_compare_by_inetaddr);
+}
+
+static void free_arg(rbnode_type *rbn, void *dummy)
+{
+    (void)dummy;
+    free(rbn);
+}
+
+void finalize_olsr_context()
+{
+    traverse_postorder(g_olsr_ctx.neighbor_link_tree, free_arg, NULL);
+    free(g_olsr_ctx.neighbor_link_tree);
+    traverse_postorder(g_olsr_ctx.neighbor_tree, free_arg, NULL);
+    free(g_olsr_ctx.neighbor_tree);
+    traverse_postorder(g_olsr_ctx.mpr_tree, free_arg, NULL);
+    free(g_olsr_ctx.mpr_tree);
+    traverse_postorder(g_olsr_ctx.selector_tree, free_arg, NULL);
+    free(g_olsr_ctx.selector_tree);
+    traverse_postorder(g_olsr_ctx.topology_tree, free_arg, NULL);
+    free(g_olsr_ctx.topology_tree);
 }
--- simnet/olsr/olsr_context.h
+++ simnet/olsr/olsr_context.h
@@ -6,8 +6,73 @@
 #include "timerqueue.h"
 #include "ringbuffer.h"
 #include "olsr_route_iface.h"
+#include "rbtree.h"
 
 #define OLSR_TX_MSGBUF_SIZE 2000
+
+typedef struct LinkElem
+{
+    rbnode_type priv_rbn;
+    in_addr_t neighbor_iface_addr; /* Rbtree Key */
+
+    uint32_t sym_time;
+    uint32_t asym_time;
+    uint32_t expire_time;
+} LinkElem;
+
+typedef struct NeighborLinkElem
+{
+    rbnode_type priv_rbn;
+    in_addr_t local_iface_addr; /* Rbtree Key */
+
+    rbtree_type *neighbor_link_tree; /* Tree of LinkElem */
+} NeighborLinkElem;
+
+typedef struct Neighbor2Elem
+{
+    rbnode_type priv_rbn;
+    in_addr_t neighbor2_main_addr; /* Rbtree Key */
+    uint32_t expire_time;
+} Neighbor2Elem;
+
+typedef struct NeighborElem
+{
+    rbnode_type priv_rbn;
+
+    in_addr_t neighbor_main_addr; /* Rbtree Key */
+    uint8_t status;
+    uint8_t willingness;
+
+    rbtree_type *neighbor2_tree; /* Tree of Neighbor2Elem */
+} NeighborElem;
+
+typedef struct MprElem
+{
+    rbnode_type priv_rbn;
+    in_addr_t mpr_addr; /* Rbtree Key */
+} MprElem;
+
+typedef struct MprSelecorElem
+{
+    rbnode_type priv_rbn; 
+    in_addr_t selector_addr; /* Rbtree Key */
+    uint32_t expire_time;
+} MprSelecorElem;
+
+typedef struct DestTopologyInfoElem
+{
+    rbnode_type priv_rbn; 
+    in_addr_t last_addr; /* Rbtree Key */
+    uint16_t seq;
+    uint32_t expire_time;
+} DestTopologyInfoElem;
+
+typedef struct TopologyInfoElem
+{
+    rbnode_type priv_rbn; 
+    in_addr_t dest_addr; /* Rbtree Key */
+    DestTopologyInfoElem last_tree;
+} TopologyInfoElem;
 
 typedef struct OlsrParam
 {
@@ -20,11 +85,17 @@ typedef struct OlsrContext
 {
     CommonRouteConfig conf;
     OlsrParam param;
-    
+
     TqCtx *timerqueue;
-    RingBuffer* olsr_tx_msgbuf;
+    RingBuffer *olsr_tx_msgbuf;
 
     uint16_t pkt_seq;
+
+    rbtree_type *neighbor_link_tree; /* Tree of NeighborLinkElem */
+    rbtree_type *neighbor_tree; /* Tree of NeighborElem */
+    rbtree_type *mpr_tree; /* Tree of MprElem */
+    rbtree_type *selector_tree; /* Tree of MprSelecorElem */
+    rbtree_type *topology_tree; /* Tree of TopologyInfoElem */
 } OlsrContext;
 
 extern OlsrContext g_olsr_ctx;
